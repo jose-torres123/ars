4943805558bb64d303579d9ad4b9fa5ff335c680
jose torres <joseprox16@gmail.com>
2020-02-29 22:04:43 -0400

    add procfie 2 231 21 12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link href="https://myappars.herokuapp.com/css/app.css" rel="stylesheet">
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <!-- Styles -->
